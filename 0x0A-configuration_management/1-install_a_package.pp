454bb0bef0d7d3c08ced8fbc9ada83e2e652fbd2
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-01-19 18:02:23 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,18 @@
 # installs the package puppet-lint
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['Werkzeug'],  # Ensure Werkzeug is installed first
 }
 
 # Address potential pip vulnerability (highly recommended):
 exec { 'upgrade_pip':
   command => 'pip3 install --upgrade pip',
   unless  => 'pip3 --version | grep -q ">=21.3"',  # Check for a more recent version
-  before  => Package['Flask'],
+  before  => Package['Werkzeug'],  # Upgrade pip before installing Werkzeug
 }
