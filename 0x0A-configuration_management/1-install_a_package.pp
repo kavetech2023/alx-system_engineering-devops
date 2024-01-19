aad12b61b5d127fe3d34980b37504a857503b71f
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-01-19 21:01:52 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,15 @@
-#!/usr/bin/pup
-# installs the package puppet-lint
+#!/usr/bin/pip
+# Ensures python3-pip is installed
 package { 'python3-pip':
   ensure   => installed,
 }
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-  require => Package['python3-pip'],
+  require  => Package['python3-pip'],
 }
 package { 'werkzeug':
   ensure   => '2.0.1',
   provider => 'pip',
-  require => Package['python3-pip'],
+  require  => Package['python3-pip'],
 }
